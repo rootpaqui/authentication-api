eec26be0dbe3251687fe4413c418f5439a3fbf1a
PAQUI <paquiromeo6@gmail.com>
2024-11-08 08:48:36 +0100

    u

--- index.mjs
+++ index.mjs
@@ -1,13 +1,18 @@
 import express from "express";
+import bodyParser from "body-parser";
 import cors from "cors";
 import sequelize from "./src/config/sequelize.mjs";
+import authRouter from "./src/routes/auth.mjs";
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+
+app.use(bodyParser.json()).use(cors());
 sequelize.initDb();
 
+app.use(authRouter);
 
 app.use((request, response) => {
   response.sendStatus(404);
