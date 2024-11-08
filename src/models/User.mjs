b11fb6f1f9eb15390b5405895649528b817821b0
PAQUI <paquiromeo6@gmail.com>
2024-11-08 07:01:23 +0100

    remove firstname & lastname from model User

--- src/models/User.mjs
+++ src/models/User.mjs
@@ -1,25 +1,21 @@
 export default (sequelize, DataTypes) => {
-  return sequelize.define("User", {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false,
-      primaryKey: true,
+  return sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: { msg: "This email already exists." },
+      },
+      password: {
+        type: DataTypes.STRING,
+      },
     },
-    firstname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    lastname: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      unique: { msg: "This email already exists." },
-    },
-    password: {
-      type: DataTypes.STRING,
-    },
-  });
+    { tableName: "users" }
+  );
 };
