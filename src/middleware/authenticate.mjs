d96f83683f0847bbe55abd86405d31c89266a84d
PAQUI <paquiromeo6@gmail.com>
2024-11-09 17:20:08 +0100

    update files

--- src/middleware/authenticate.mjs
+++ src/middleware/authenticate.mjs
@@ -3,24 +3,12 @@ import jwt from "jsonwebtoken";
 export default (request, response, next) => {
   const authorizationHeader = request.headers.authorization;
 
-  if (!authorizationHeader) {
-    const message = `Authentication token is missing.`;
-    return response.status(401).json({ message });
-  }
+  if (!authorizationHeader) return response.sendStatus(401);
 
   const token = authorizationHeader.split(" ")[1];
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decodedToken) => {
-    if (error) {
-      const message = `Invalid token.`;
-      return response.status(401).json({ message, data: error });
-    }
-
-    const userId = decodedToken.userId;
-    if (request.body.userId && request.body.userId !== userId) {
-      const message = `User is not valid`;
-      res.status(401).json({ message });
-    } else {
-      next();
-    }
+    if (error) return response.sendStatus(403);
+    request.user = decodedToken.userId;
+    next();
   });
 };
